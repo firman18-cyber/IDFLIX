c01a3e407139e633a2d4001d63ab52ec0435ea37
firman18-cyber <67278774+firman18-cyber@users.noreply.github.com>
2026-09-25 00:03:37 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB2eAOxLs6ri2bmuI30FnAUGCDC8EVLimk",
+  authDomain: "idflix-219d7.firebaseapp.com",
+  databaseURL: "https://idflix-219d7-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "idflix-219d7",
+  storageBucket: "idflix-219d7.firebasestorage.app",
+  messagingSenderId: "576659426224",
+  appId: "1:576659426224:web:a804b3fded2b17f42de475"
+};
