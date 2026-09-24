062ca24796e1a375e03dcc7d061acc7a5887151f
firman18-cyber <67278774+firman18-cyber@users.noreply.github.com>
2026-09-25 00:02:53 +0700

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-// Isi dari Firebase Console > Project settings > Your apps (Web). Boleh publik; keamanan dijaga oleh database.rules.json.
-window.FIREBASE_CONFIG = {
-  apiKey: "ISI_API_KEY",
-  authDomain: "PROJECT.firebaseapp.com",
-  databaseURL: "https://PROJECT-default-rtdb.firebaseio.com",
-  projectId: "PROJECT",
-  appId: "ISI_APP_ID"
-};
